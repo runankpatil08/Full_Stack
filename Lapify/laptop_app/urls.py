84bd0965317f46a5c56b7d3bea30860198c7f8e2
runankpatil08 <runankpatil08@gmail.com>
2025-08-21 18:23:10 +0530

    basic CRUD complete

--- Lapify/laptop_app/urls.py
+++ Lapify/laptop_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import add_view,show_view
+from .views import add_view,show_view,update_view,delete_view
 
 
 urlpatterns=[
     path("add/",add_view,name='add'),
-    path("show/",show_view,name='show')
+    path("show/",show_view,name='show'),
+    path("update/<int:id>/",update_view,name='update'),
+    path("delete/<int:id>/",delete_view,name='delete')
 ]
--- Lapify/laptop_app/views.py
+++ Lapify/laptop_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import LaptopForm
 from .models import Laptop
 
@@ -17,3 +17,21 @@ def show_view(request):
     template_name='laptop_app/show_laptop.html'
     context={"laptop":laptop}
     return render(request, template_name, context)
+
+def update_view(request,id):
+    laptop=Laptop.objects.get(id=id)
+    form=LaptopForm(instance=laptop)
+    if request.method=="POST":
+        form=LaptopForm(request.POST,instance=laptop)
+        if form.is_valid():
+            form.save()
+            return redirect('show')
+    template_name='laptop_app/add_laptop.html'
+    context={'form':form}
+    return render(request, template_name, context)
+
+
+def delete_view(request,id):
+    laptop=Laptop.objects.get(id=id)
+    laptop.delete()
+    return redirect('show')
